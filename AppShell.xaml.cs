8204a41a6682bbda0dbdd49cb369f17513a6d64d
ViniciusCgama <coelhovinigama@gmail.com>
2023-07-28 22:45:17 -0300

    first commit

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace notes;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ sobre.xaml.cs
@@ -0,0 +1,16 @@
+namespace notes;
+
+public partial class sobre : ContentPage
+{
+	public sobre()
+	{
+		InitializeComponent();
+	}
+
+    private async void LearnMore_Clicked(object sender, EventArgs e)
+    {
+        // Navigate to the specified URL in the system browser.
+        await Launcher.Default.OpenAsync("https://aka.ms/maui");
+    }
+
+}
